6271c45a41fcdf5180e84c9c72f46dff88cdcf70
Carlos <cfpc141@gmail.com>
2020-02-12 16:16:24 +1300

    Movement and Dash Start

---
+++ LimboStrikers/Assets/Dashscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dashscript : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    private float timer = 0.2f;
+    private GameObject Player;
+
+    void Start()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        Player = GameObject.Find("Player");
+        transform.position = Player.transform.position;
+        transform.localScale = Player.transform.localScale;
+        sprite.sprite = Player.GetComponent<MyCharacterController>().playerSprite.sprite;
+        sprite.color = new Vector4(50, 50, 50, 0.2f);
+
+    }
+
+    void FixedUpdate()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- LimboStrikers/Assets/MyCharacterController.cs
+++ LimboStrikers/Assets/MyCharacterController.cs
@@ -2,17 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class MyCharacterController : MonoBehaviour
 {
+    public GameObject playerghost;
+    private Rigidbody2D rb;
+    private Vector2 move;
+    public float speed;
+    private bool click;
+    public bool dash = false;
+    public bool dashready = false;
+    public float dashTimer;
+    public float startdashTimer;
+    public float dashcooldownTimer;
+    public float startcooldwondashTimer;
+    bool dashrecover;
+    public SpriteRenderer playerSprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        click = Input.GetButtonDown("Fire1");
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(move.x, move.y);
+        if(!click)
+        rb.velocity = movement * speed;
+        dashingfunc();
+    }
+
+    void dashingfunc()
+    {
+        Vector2 movement2 = new Vector2(move.x, move.y);
+     
+            if (click)
+            {
+                GameObject ghostie = Instantiate(playerghost, transform.position, transform.rotation);
+                dashTimer -= Time.deltaTime;
+                rb.velocity = movement2 * speed * 2f;
+            }
         
+
     }
 }
