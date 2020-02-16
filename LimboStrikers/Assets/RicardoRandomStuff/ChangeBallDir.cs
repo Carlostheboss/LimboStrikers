a98e5e9171aed3acd361b4c8cfac13b73489280b
Ricardo Jorge <50033676@europeia.pt>
2020-02-16 23:34:18 +0000

    Thingy that changes ball direction

---
+++ LimboStrikers/Assets/RicardoRandomStuff/ChangeBallDir.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBallDir : MonoBehaviour
+{
+
+    float DestroyTimer;
+    float TimeToDestroy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DestroyTimer = Time.time;
+        TimeToDestroy = DestroyTimer + 15.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimeToDestroy < Time.time)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "ball")
+        {
+            Rigidbody2D rb2d = other.GetComponent<Rigidbody2D>();
+            float randVal = Random.Range(-1, 1.1f);
+
+            Vector3 ballT = new Vector3(0, randVal, 0);
+
+
+
+            rb2d.AddForce(ballT * 20.0f, ForceMode2D.Impulse);
+
+
+            Destroy(gameObject);
+        }
+    }
+}
